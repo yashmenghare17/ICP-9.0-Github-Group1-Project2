9efb6d460d6f21cea132b95642ff85cd006e25bf
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2024-11-27 14:20:19 +0530

    added slide  next function

---
+++ js/register.js
@@ -0,0 +1,15 @@
+const images = ["./../img/register-curosal-img1.jpg","./../img/register-curosal-img2.jpg" ,"./../img/register-curosal-img3.jpg",
+    "./../img/register-curosal-img4.jpg", "./../img/register-cursoal-img5.jpg"]
+  let index = 0;
+  const slide = document.getElementById('slide');
+  function next()
+  {
+    if (index < images.length-1)
+    {
+        index++;
+    }
+    else{
+        index = 0;
+    }
+       slide.src = images[index]
+  }
