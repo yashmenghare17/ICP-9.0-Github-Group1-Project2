ef7d653db3fce3f1374fd825cf1facf2250f16d0
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2024-11-28 10:12:50 +0530

    curosal created

--- js/register.js
+++ js/register.js
@@ -1,31 +1,32 @@
-const images = ["./../img/register-curosal-img1.jpg","./../img/register-curosal-img2.jpg" ,"./../img/register-curosal-img3.jpg",
-    "./../img/register-curosal-img4.jpg", "./../img/register-cursoal-img5.jpg"]
-  let index = 0;
-  const slide = document.getElementById('slide');
-  function next()
-  {
-    if (index < images.length-1)
-    {
-        index++;
+let curosal_data = document.getElementById("curosal_img");
+const curosalImgArray = ['home-curosal-img1','home-curosal-img2','home-curosal-img3','home-curosal-img4','home-curosal-img5'];
+let count =0;
+function prevNext(type){
+    if(type=='Prev'){
+        count--;
+        if(count < 0){
+            count=curosalImgArray.length-1;
+        }
+        
+    }else{
+        count++;
+        if(count >= curosalImgArray.length){
+            count=0;
+        }
     }
-    else{
-        index = 0;
-    }
-       slide.src = images[index]
-  }
-  function prev()
-  {
-      if(index > 0){
-  
-      
-          index--;
-       }
-       else{
-           index = images.length -1;
-       }
-          slide.src = images[index]
-  }
-  setInterval(next, 2000);
+    curosal_data.innerHTML = `<img src="./../img/${curosalImgArray[count]}.jpg"  alt="${curosalImgArray[count]}" />`;
+}
+setInterval(function(){
+    prevNext('Next');
+},5000);
+
+
+
+
+
+
+
+
 
   function openModel()
   {
@@ -44,3 +45,4 @@ function okButton()
     const overlayElement = document.getElementById('overlay');
     overlayElement.style.display = 'none';
 }
+
