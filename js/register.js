0d4a7a35a7597435235f65f5ec320295c4af18ba
unknown <yashmenghare17@gmail.com>
2024-11-30 22:32:27 +0530

    add new update

--- js/register.js
+++ js/register.js
@@ -1,47 +1,34 @@
 let curosal_data = document.getElementById("curosal_img");
-const curosalImgArray = ['home-curosal-img1','home-curosal-img2','home-curosal-img3','home-curosal-img4','home-curosal-img5'];
-let count =0;
-function prevNext(type){
-    if(type=='Prev'){
+const curosalImgArray = ['home-curosal-img1', 'home-curosal-img2', 'home-curosal-img3', 'home-curosal-img4', 'home-curosal-img5'];
+let count = 0;
+function prevNext(type) {
+    if (type == 'Prev') {
         count--;
-        if(count < 0){
-            count=curosalImgArray.length-1;
+        if (count < 0) {
+            count = curosalImgArray.length - 1;
         }
-        
-    }else{
+
+    } else {
         count++;
-        if(count >= curosalImgArray.length){
-            count=0;
+        if (count >= curosalImgArray.length) {
+            count = 0;
         }
     }
     curosal_data.innerHTML = `<img src="./../img/${curosalImgArray[count]}.jpg"  alt="${curosalImgArray[count]}" />`;
 }
-setInterval(function(){
+setInterval(function () {
     prevNext('Next');
-},5000);
+}, 5000);
+function openModel() {
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'flex';
 
-
-
-
-
-
-
-
-
-  function openModel()
-  {
-      const overlayElement = document.getElementById('overlay');
-      overlayElement.style.display = 'flex';
-  
-  }
-
-  function closeModel()
-{
+}
+function closeModel() {
     const overlayElement = document.getElementById('overlay');
     overlayElement.style.display = 'none';
 }
-function okButton()
-{
+function okButton() {
     const overlayElement = document.getElementById('overlay');
     overlayElement.style.display = 'none';
 }
