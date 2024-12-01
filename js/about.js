94853a212859df3a4df0cdfb1b374847293f81e6
chaitaligarud <garudchaitali60@gmail.com>
2024-12-01 16:36:12 +0530

    added content in about us page

---
+++ js/about.js
@@ -0,0 +1,35 @@
+function changeImage(num)
+{
+    const mainImage=document.getElementById('mainImage');
+    if(num===1){
+        mainImage.src="./img/pic2.jpg";
+      
+
+
+    }
+  else if(num===2){
+        mainImage.src="./../img/pic3.jpg";
+        
+    }
+   else if(num===3){
+        mainImage.src="./../img/pic4.jpg";
+        
+    }
+    else if(num===4){
+        mainImage.src="./../img/pic5.jpg";
+        
+    }
+    else if(num===5){
+        mainImage.src="./../img/pic6.jpg";
+        
+    } else if(num===6){
+        mainImage.src="./../img/pic7.jpg";
+        
+    }
+    else if(num===7){
+        mainImage.src="./../img/pic8.jpg";
+        
+    }
+
+}
+
